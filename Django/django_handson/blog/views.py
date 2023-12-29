5d67a195e5c67cddd1224a9baa523bd78c9cdfa4
Shyam Goli <golishyamgolisai@gmail.com>
2023-12-29 11:46:52 +0530

    initial stages of django

--- Django/django_handson/blog/views.py
+++ Django/django_handson/blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author':"SS. Rajamouli",
-        'title':"Bahubali",
-        'content':"This is the epic movie",
-        'Posted_date':"May 31, 2020"
-    },
-    {
-        'author':"RGV",
-        'title':"Rakthacharittra",
-        'content':"This is the bloodshed movie",
-        'posted_date':"August 31, 2010"
-    }
+# posts = [
+#     {
+#         'author':"SS. Rajamouli",
+#         'title':"Bahubali",
+#         'content':"This is the epic movie",
+#         'posted_date':"May 31, 2020"
+#     },
+#     {
+#         'author':"RGV",
+#         'title':"Rakthacharittra",
+#         'content':"This is the bloodshed movie",
+#         'posted_date':"August 31, 2010"
+#     }
 
-]
+# ]
 def home(request):
     # return HttpResponse("<h1>Welcome to Home Page.</h1>")
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
