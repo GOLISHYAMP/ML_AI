e12f9d1098e6137357d344a6e6d58ae6e34fe1af
Shyam Goli <golishyamgolisai@gmail.com>
2023-12-31 18:10:35 +0530

    some modifications

--- Django/django_handson/user_login/views.py
+++ Django/django_handson/user_login/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .form import UserRegisterForm
 # Create your views here.
 def user_register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog_home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'user_login/register.html', {'form':form})
