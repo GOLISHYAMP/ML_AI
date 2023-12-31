a69e316cabec3fd0776016fb6fd7dfb9df9cd9e1
Shyam Goli <golishyamgolisai@gmail.com>
2023-12-31 19:51:04 +0530

    modified

--- Django/django_handson/django_handson/urls.py
+++ Django/django_handson/django_handson/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user_login import views as login_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('register/',login_views.user_register, name = "user_register_view"),
+    path('register/',login_views.user_register, name = "register"),
+    path('login/',auth_views.LoginView.as_view(template_name = "user_login/login.html"), name = "login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name = "user_login/logout.html"), name = "logout"),
+    
 ]
