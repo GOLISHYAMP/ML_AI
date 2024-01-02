356a4717b0df68484ac525a08c5a4d625c66f756
Shyam Goli <golishyamgolisai@gmail.com>
2024-01-02 11:02:33 +0530

    new added

--- Django/django_handson/django_handson/urls.py
+++ Django/django_handson/django_handson/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from user_login import views as login_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +27,8 @@ urlpatterns = [
     path('register/',login_views.user_register, name = "register"),
     path('login/',auth_views.LoginView.as_view(template_name = "user_login/login.html"), name = "login"),
     path('logout/',auth_views.LogoutView.as_view(template_name = "user_login/logout.html"), name = "logout"),
-    
+    path('profile/',login_views.profile,name = 'profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Django/django_handson/user_login/views.py
+++ Django/django_handson/user_login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .form import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def user_register(request):
     if request.method == "POST":
@@ -9,8 +10,12 @@ def user_register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog_home')
+            messages.success(request, f'Your Account created has been created and now You are able to login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user_login/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'user_login/profile.html')
