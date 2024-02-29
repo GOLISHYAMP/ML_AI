e0010b89c2b9902df1f08361c52b3e2f013728b8
Shyam Goli <golishyamgolisai@gmail.com>
2024-02-29 19:20:40 +0530

    some updates

--- Django/DjRevise/post/models.py
+++ Django/DjRevise/post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Post(models.Model):
+    Heading = models.TextField()
+    Author = models.ForeignKey(User,on_delete = models.CASCADE)
+    PostOn = models.DateTimeField(auto_now_add=True)
+    Description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.Heading
 
 
--- Django/DjRevise/post/views.py
+++ Django/DjRevise/post/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-from post import views as post_view
+from .models import Post
 # Create your views here.
 
 def home(request):
-    posts = [
-    {
-        'Author':"Shyam Goli",
-        'PostedOn':"Jan 16, 2024",
-        'Heading':"Buy 22000 CE Nifty50",
-        'Description':"Buying volums are increasing and the graph is just crossing the 30 EMA line."
-    },
-    {
-        'Author':"Vinod Sarwade",
-        'PostedOn':"Jan 15, 2024",
-        'Heading':"BUY 21900 PE Nifty50",
-        'Description':"SMC Strong high level has been touched and graph coming down."
-    }
-    ]
+    # posts = [
+    # {
+    #     'Author':"Shyam Goli",
+    #     'PostedOn':"Jan 16, 2024",
+    #     'Heading':"Buy 22000 CE Nifty50",
+    #     'Description':"Buying volums are increasing and the graph is just crossing the 30 EMA line."
+    # },
+    # {
+    #     'Author':"Vinod Sarwade",
+    #     'PostedOn':"Jan 15, 2024",
+    #     'Heading':"BUY 21900 PE Nifty50",
+    #     'Description':"SMC Strong high level has been touched and graph coming down."
+    # }
+    # ]
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'post/home.html',context)
 
